7e5320dd4e3a9fc408da629284f587cb6b9886dc
kambangsinclaire <kambangsinclaire@gmail.com>
2023-07-12 13:20:49 +0100

    showcase project setup - Bindings in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+// Compoenent Decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'showcase';
+
+  name: string = "Our Showcase App";
+  description: string = "The best Angular showcase software"
+  creators: any = { name: ['Mr Kambang', "Web Legends"] };
+  age = 40;
+  price = 300;
+
+  username: string = "";
+
+  sayHi() {
+    return "Hello " + this.name + " how are you?"
+  }
+
 }
